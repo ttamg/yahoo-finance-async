af0d15235034e44dd093b67d5416606332440a1c
Matt Gosden <mdgosden@gmail.com>
2022-05-06 09:16:16 +0300

    Release 0.1.2

--- yahoo_finance_async/__init__.py
+++ yahoo_finance_async/__init__.py
@@ -1,5 +1,5 @@
 """A Python async API wrapper for the deprecated (but currently still working) Yahoo Finance API"""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
-from .api import OHLC, Interval, History
+from .api import OHLC, History, Interval
