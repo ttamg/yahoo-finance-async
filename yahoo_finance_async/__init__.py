4bbf9f54be3de846cda6ee4f9c4b5528e7edd4e2
Matt Gosden <mdgosden@gmail.com>
2025-02-24 08:03:35 +0000

    Fixes for Yahoo API changes requiring User Agent

--- yahoo_finance_async/__init__.py
+++ yahoo_finance_async/__init__.py
@@ -1,5 +1,5 @@
 """A Python async API wrapper for the deprecated (but currently still working) Yahoo Finance API"""
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 from .api import OHLC, History, Interval
--- yahoo_finance_async/api.py
+++ yahoo_finance_async/api.py
@@ -1,6 +1,6 @@
-import asyncio
 import datetime
 import json
+import random
 import warnings
 from enum import Enum
 
@@ -43,6 +43,22 @@ class Interval(Enum):
     THREE_MONTH = "3mo"
 
 
+USER_AGENTS = [
+    # Chrome
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+    # Firefox
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.7; rv:135.0) Gecko/20100101 Firefox/135.0",
+    "Mozilla/5.0 (X11; Linux i686; rv:135.0) Gecko/20100101 Firefox/135.0",
+    # Safari
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_7_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.3 Safari/605.1.15",
+    # Edge
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/131.0.2903.86",
+]
+
+
 class OHLC:
     """API call to fetch the OHLC candles."""
 
@@ -50,6 +66,7 @@ class OHLC:
     base_params = {
         "events": "history",
     }
+    headers = {"User-Agent": random.choice(USER_AGENTS)}
 
     @classmethod
     async def fetch(
@@ -104,7 +121,7 @@ class OHLC:
 
         async with aiohttp.ClientSession() as session:
 
-            async with session.get(url, params=params) as response:
+            async with session.get(url, params=params, headers=cls.headers) as response:
 
                 try:
                     response = await response.text()
